17b230902e88481188b9e31af564cbec06959780
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-10-06 02:53:28 -0300

    chore: create a scaffold with plop to new routes

---
+++ plop/index.js
@@ -0,0 +1,47 @@
+// import { NodePlopAPI } from 'plop';
+module.exports = (plop) => {
+  plop.setHelper('lowerCaseNoSpaces', function (text) {
+    return text.toLowerCase().replace(/\s/gi, '');
+  });
+
+  plop.setGenerator('object', {
+    description: 'Create everything for a new Route',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter new Route Name',
+      },
+    ],
+    actions: (data) => {
+      let defaultPath = '../src/{{pascalCase name}}';
+      const nameLowerCaseNoSpaces = data.name.toLowerCase().replace(/\s/gi, '');
+      return [
+        // Route
+        {
+          type: 'add',
+          path: `${defaultPath}/{{camelCase name}}.route.ts`,
+          templateFile: '../plop-templates/routes.ts.hbs',
+        },
+        // Model
+        {
+          type: 'add',
+          path: `${defaultPath}/model.ts`,
+          templateFile: '../plop-templates/model.ts.hbs',
+        },
+        // Service
+        {
+          type: 'add',
+          path: `${defaultPath}/service.ts`,
+          templateFile: '../plop-templates/service.ts.hbs',
+        },
+        // Controller
+        {
+          type: 'add',
+          path: `${defaultPath}/controller.ts`,
+          templateFile: '../plop-templates/controller.ts.hbs',
+        },
+      ];
+    },
+  });
+};
