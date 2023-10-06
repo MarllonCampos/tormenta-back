cc733ae48bb360e53192f7118ede4504b0186b58
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-10-06 02:58:15 -0300

    feat: add new route for docs and add swagger to project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express, { Response } from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import { configDotenv } from 'dotenv';
 import { routes } from './routes';
+import swaggerDocument from './swagger.json';
+import swaggerJSDoc from 'swagger-jsdoc';
+
 configDotenv();
 const app = express();
 const serverRoutes = routes;
@@ -11,6 +15,8 @@ app.use(routes);
 app.get('/', (_, res: Response) => {
   res.send('Server Working Just Fine');
 });
+const specs = swaggerJSDoc(swaggerDocument);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.listen(process.env.PORT, () => {
   console.log(`🔥 Server Running on Port: ${process.env.PORT} 🔥`);
 });
