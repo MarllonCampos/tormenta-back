89d5baa370c2e43f7a125f5070b0fe1766164ff7
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-10-07 02:07:29 -0300

    feat: implement index and show methods, to return one weapon and all weapons

--- src/Weapon/controller.ts
+++ src/Weapon/controller.ts
@@ -1,4 +1,3 @@
-import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
 import WeaponService from './service';
 
@@ -8,16 +7,20 @@ class WeaponController {
     this.service = new WeaponService();
   }
 
-  index = async (req: Request, res: Response) => {
+  index = async (_: Request, res: Response) => {
     const weapons = await this.service.index();
     return res.json(weapons);
   };
-  async show(req: Request, res: Response) {
-    return null;
-  }
-  async store(req: Request, res: Response) {
-    return null;
-  }
+  show = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    console.log(id);
+
+    if (!id) return res.status(400).send('You burro men?!');
+    const formattedId = Number(id);
+    const specificWeapon = await this.service.show(formattedId);
+    res.status(200).send(specificWeapon);
+  };
+  async store(req: Request, res: Response) {}
   async update(req: Request, res: Response) {
     return null;
   }
