617e0d2d931906abd4d84043a9a7969df8edff7b
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-10-11 03:50:38 -0300

    feat: add range to the routes of the app

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { WeaponRoutes } from './Weapon/weapon.route';
+import { RangeRoutes } from './Range/range.route';
 
 const routes = Router();
 
 routes.use('/weapon', WeaponRoutes);
+routes.use('/range', RangeRoutes);
 
 export { routes };
