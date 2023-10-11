b0933e9d8ab7ebef6f18e30fc997cb8b64a5b919
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-10-10 21:18:24 -0300

    feat: create a default error handling middleware

--- src/Weapon/weapon.route.ts
+++ src/Weapon/weapon.route.ts
@@ -1,6 +1,6 @@
-import { Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import WeaponController from './controller';
-
+import WeaponErrors from './errors';
 const WeaponRoutes = Router();
 
 const controller = new WeaponController();
@@ -34,5 +34,21 @@ WeaponRoutes.post('/', controller.store);
 WeaponRoutes.patch('/:id', controller.update);
 
 WeaponRoutes.delete('/:id', controller.delete);
+
+WeaponRoutes.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  if (err instanceof WeaponErrors) {
+    const sendObject: {
+      message: string;
+      details?: string[];
+    } = {
+      message: err.message,
+    };
+    if (err.errors.length > 0) sendObject.details = err.errors;
+    return res.status(400).send(sendObject);
+  }
+
+  console.log('Error Middleware', err, err.constructor.name);
+  res.status(500).send({ message: 'Erro interno no servidor' });
+});
 
 export { WeaponRoutes };
