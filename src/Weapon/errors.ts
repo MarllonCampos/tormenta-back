0a7d803ecd27374976f941ad790cfc7c6dc28267
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-10-10 21:17:24 -0300

    feat: create a error class to create default errors for weapons

---
+++ src/Weapon/errors.ts
@@ -0,0 +1,25 @@
+export default class WeaponErrors extends Error {
+  errors: string[] = [];
+
+  constructor(message: string, errors: string[] = []) {
+    super(message);
+    this.errors = errors;
+    Object.setPrototypeOf(this, WeaponErrors.prototype);
+  }
+
+  static ValidationErrors(errors: string[] = []): WeaponErrors {
+    return new WeaponErrors('Houve um erro com a validação dos dados', errors);
+  }
+
+  static DefaultErrorMessage(errors: string[] = []): WeaponErrors {
+    return new WeaponErrors('Existem erros a serem corrigidos, verifique e corrija-os', errors);
+  }
+
+  static WeaponNotFound(): WeaponErrors {
+    return new WeaponErrors('A arma informada não foi encontrada não foi encontrada');
+  }
+
+  static NoFieldsToUpdate(): WeaponErrors {
+    return new WeaponErrors('Não há campos a serem alterados');
+  }
+}
