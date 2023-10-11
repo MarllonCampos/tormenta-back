3514a9a1753aca6f7621a9516e0d79eb97f8ee39
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-10-11 02:44:17 -0300

    feat: create rangeDTO

--- src/Range/model.ts
+++ src/Range/model.ts
@@ -1,1 +1,42 @@
-export class RangeModel {}
+import * as yup from 'yup';
+
+export interface rangeInputInterface {
+  type: string;
+}
+export default class RangeDTO {
+  private _range: any;
+  private rangeSchema = yup.object().shape({
+    type: yup
+      .string()
+      .min(3, 'O campo de nome [type]  deve ter no mínimo 3 caracteres')
+      .required('O campo de nome [type] não deve estar vazio'),
+  });
+
+  private _defaultYupOptions = {
+    abortEarly: false,
+    stripUnknown: true,
+  };
+
+  constructor(range: any) {
+    this._range = this.rangeSchema.cast(range);
+  }
+
+  create = (): rangeInputInterface => {
+    const validateNewRange = this.rangeSchema.validateSync(this._range, this._defaultYupOptions);
+
+    return validateNewRange;
+  };
+
+  update = (): rangeInputInterface => {
+    const validatedUpdateRange = this.rangeSchema.validateSync(this._range, this._defaultYupOptions);
+    return validatedUpdateRange;
+  };
+
+  view = (): yup.InferType<typeof this.rangeSchema> => {
+    const castedWeapon = this.rangeSchema.cast(this._range, {
+      stripUnknown: true,
+    });
+
+    return castedWeapon;
+  };
+}
