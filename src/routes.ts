e17a365686838d72be88302a456e1d294d8eb357
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-10-12 16:04:30 -0300

    feat: add damageType to the routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import { WeaponRoutes } from './Weapon/weapon.route';
 import { RangeRoutes } from './Range/range.route';
 import { HoldTypeRoutes } from './HoldType/holdType.route';
+import { DamageTypeRoutes } from './DamageType/damageType.route';
 
 const routes = Router();
 
 routes.use('/weapon', WeaponRoutes);
 routes.use('/range', RangeRoutes);
 routes.use('/holdtype', HoldTypeRoutes);
+routes.use('/damagetype', DamageTypeRoutes);
 
 export { routes };
